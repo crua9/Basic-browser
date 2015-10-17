d46d3f4f2387fb96b8f8385e55bb515e75c11815
crua9 <ctbennettii@gmail.com>
2015-10-16 20:23:17 -0400

    Make the app
    Making the app

---
+++ Tech Reviews and Help/Tech Reviews and Help/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Tech_Reviews_and_Help
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Hacking = new System.Windows.Forms.Button();
+            this.Coding = new System.Windows.Forms.Button();
+            this.Windows = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.Forward = new System.Windows.Forms.Button();
+            this.Go = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.videoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpUsGrowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.payPalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.patreonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.youTubeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Hacking
+            // 
+            this.Hacking.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Hacking.BackgroundImage")));
+            this.Hacking.Location = new System.Drawing.Point(12, 105);
+            this.Hacking.Name = "Hacking";
+            this.Hacking.Size = new System.Drawing.Size(101, 23);
+            this.Hacking.TabIndex = 0;
+            this.Hacking.Text = "Hacking videos";
+            this.Hacking.UseVisualStyleBackColor = true;
+            this.Hacking.Click += new System.EventHandler(this.Hacking_Click);
+            // 
+            // Coding
+            // 
+            this.Coding.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Coding.BackgroundImage")));
+            this.Coding.Location = new System.Drawing.Point(12, 315);
+            this.Coding.Name = "Coding";
+            this.Coding.Size = new System.Drawing.Size(101, 23);
+            this.Coding.TabIndex = 1;
+            this.Coding.Text = "Coding videos";
+            this.Coding.UseVisualStyleBackColor = true;
+            this.Coding.Click += new System.EventHandler(this.Coding_Click);
+            // 
+            // Windows
+            // 
+            this.Windows.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Windows.BackgroundImage")));
+            this.Windows.Location = new System.Drawing.Point(12, 575);
+            this.Windows.Name = "Windows";
+            this.Windows.Size = new System.Drawing.Size(101, 23);
+            this.Windows.TabIndex = 2;
+            this.Windows.Text = "Windows videos";
+            this.Windows.UseVisualStyleBackColor = true;
+            this.Windows.Click += new System.EventHandler(this.Windows_Click);
+            // 
+            // Back
+            // 
+            this.Back.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Back.BackgroundImage")));
+            this.Back.Location = new System.Drawing.Point(133, 24);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 3;
+            this.Back.Text = "<-";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Forward
+            // 
+            this.Forward.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Forward.BackgroundImage")));
+            this.Forward.Location = new System.Drawing.Point(214, 25);
+            this.Forward.Name = "Forward";
+            this.Forward.Size = new System.Drawing.Size(75, 23);
+            this.Forward.TabIndex = 4;
+            this.Forward.Text = "->";
+            this.Forward.UseVisualStyleBackColor = true;
+            this.Forward.Click += new System.EventHandler(this.Forward_Click);
+            // 
+            // Go
+            // 
+            this.Go.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Go.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Go.BackgroundImage")));
+            this.Go.Location = new System.Drawing.Point(737, 27);
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(75, 23);
+            this.Go.TabIndex = 5;
+            this.Go.Text = "Go";
+            this.Go.UseVisualStyleBackColor = true;
+            this.Go.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 724);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(824, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.Lime;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Status";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.ForeColor = System.Drawing.Color.Aqua;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel2.Text = "l";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(49, 17);
+            this.toolStripStatusLabel3.Text = "Website";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(119, 24);
+            this.label1.MinimumSize = new System.Drawing.Size(8, 700);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(8, 700);
+            this.label1.TabIndex = 7;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(133, 56);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(691, 668);
+            this.webBrowser1.TabIndex = 8;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser1_ProgressChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.helpUsGrowToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(824, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.videoToolStripMenuItem,
+            this.gitToolStripMenuItem});
+            this.aboutToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // videoToolStripMenuItem
+            // 
+            this.videoToolStripMenuItem.Name = "videoToolStripMenuItem";
+            this.videoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.videoToolStripMenuItem.Text = "Video";
+            // 
+            // gitToolStripMenuItem
+            // 
+            this.gitToolStripMenuItem.Name = "gitToolStripMenuItem";
+            this.gitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gitToolStripMenuItem.Text = "Git";
+            this.gitToolStripMenuItem.Click += new System.EventHandler(this.gitToolStripMenuItem_Click);
+            // 
+            // helpUsGrowToolStripMenuItem
+            // 
+            this.helpUsGrowToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.payPalToolStripMenuItem,
+            this.patreonToolStripMenuItem,
+            this.youTubeToolStripMenuItem});
+            this.helpUsGrowToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.helpUsGrowToolStripMenuItem.Name = "helpUsGrowToolStripMenuItem";
+            this.helpUsGrowToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.helpUsGrowToolStripMenuItem.Text = "Help us grow";
+            // 
+            // payPalToolStripMenuItem
+            // 
+            this.payPalToolStripMenuItem.Name = "payPalToolStripMenuItem";
+            this.payPalToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.payPalToolStripMenuItem.Text = "PayPal";
+            this.payPalToolStripMenuItem.Click += new System.EventHandler(this.payPalToolStripMenuItem_Click);
+            // 
+            // patreonToolStripMenuItem
+            // 
+            this.patreonToolStripMenuItem.Name = "patreonToolStripMenuItem";
+            this.patreonToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.patreonToolStripMenuItem.Text = "Patreon";
+            this.patreonToolStripMenuItem.Click += new System.EventHandler(this.patreonToolStripMenuItem_Click);
+            // 
+            // youTubeToolStripMenuItem
+            // 
+            this.youTubeToolStripMenuItem.Name = "youTubeToolStripMenuItem";
+            this.youTubeToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.youTubeToolStripMenuItem.Text = "YouTube";
+            this.youTubeToolStripMenuItem.Click += new System.EventHandler(this.youTubeToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(295, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(436, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "https://www.youtube.com/user/TechRHelp";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(824, 746);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.Go);
+            this.Controls.Add(this.Forward);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.Windows);
+            this.Controls.Add(this.Coding);
+            this.Controls.Add(this.Hacking);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Hacking;
+        private System.Windows.Forms.Button Coding;
+        private System.Windows.Forms.Button Windows;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Forward;
+        private System.Windows.Forms.Button Go;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem helpUsGrowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem payPalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripMenuItem videoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem patreonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem youTubeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gitToolStripMenuItem;
+    }
+}
+
---
+++ Tech Reviews and Help/Tech Reviews and Help/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tech_Reviews_and_Help
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("https://www.youtube.com/user/TechRHelp");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //GoToSite
+        private void GoToSite()
+        {
+            webBrowser1.Navigate(textBox1.Text);
+            textBox1.Enabled = false;
+            Go.Enabled = false;
+        }
+        // Go button
+        private void Go_Click(object sender, EventArgs e)
+        {
+            GoToSite();
+        }
+
+        private void Forward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+        //hacking
+        private void Hacking_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/playlist?list=PLioOFeHwQV5wT-55UfUP2w14miGVeg15h");
+        }
+
+        private void Coding_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/playlist?list=PLioOFeHwQV5wqTf9S43CmwNml4UBXYqkT");
+        }
+
+        private void Windows_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/playlist?list=PLioOFeHwQV5yqxT4gO7nGrX04hW2lXoy4");
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            textBox1.Enabled = true;
+            Go.Enabled = true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter) 
+            {
+                Go_Click(null, null);
+            }
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.CurrentProgress > 0 && e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+                textBox1.Text = webBrowser1.Url.ToString();
+                toolStripStatusLabel3.Text = webBrowser1.Url.ToString();           
+                     }
+        }
+
+        private void payPalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://techreviewsandhelp.com/l2u5");
+        }
+
+        private void patreonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+       
+            webBrowser1.Navigate("https://www.patreon.com/user?u=381858&ty=h");
+        }
+
+        private void youTubeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/user/TechRHelp");
+        }
+
+        private void gitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://github.com/crua9/Basic-browser");
+        }
+    }
+
+}
